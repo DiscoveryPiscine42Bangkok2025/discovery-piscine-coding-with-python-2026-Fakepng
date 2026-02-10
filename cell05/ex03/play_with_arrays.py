14539149b501668e6ec902ef7f6472fd7245ce2d
Krit Kasemtewin <krit0kasemtewin@gmail.com>
2026-02-11 04:07:01 +0700

    cell05/ex03

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for element in original_array:
+    if element > 5:
+        new_array.append(element + 2)
+
+print(original_array)
+print(set(new_array))
