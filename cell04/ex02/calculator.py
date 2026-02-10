ddc321aafc28f272872ccf77d2b922dab2bfd2d1
Krit Kasemtewin <krit0kasemtewin@gmail.com>
2026-02-11 03:56:26 +0700

    cell04/ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+first_number = int(input("Give me the first number: "))
+second_number = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} / {second_number} = {(first_number / second_number):.0f}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
